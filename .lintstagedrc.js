d421123a94e7ddf22ecfad0b2e038583c3bd6b98
3dw1nM0535 <workockmoses@gmail.com>
2023-06-24 03:23:10 +0300

    lint setup

---
+++ .lintstagedrc.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+const buildEslintCommand = filenames => {
+  const files = filenames.filter(f => !f.includes('public/')).map(f => path.relative(process.cwd(), f))
+  return files?.length > 0
+    ? `next lint --fix --file ${filenames.filter(f => !f.includes('public/')).map(f => path.relative(process.cwd(), f)).join(' --file ')}`
+    : `next lint --fix`
+}
+
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+}

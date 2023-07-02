969f9922e8888f7bc9300e69c3c9978c3e4d1bf5
3dw1nM0535 <workockmoses@gmail.com>
2023-07-03 02:22:46 +0300

    chore: last minute

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -2,6 +2,7 @@
 
 import { AbsoluteCenter, Container, Text } from "@chakra-ui/react";
 import Head from "next/head";
+import { TypeAnimation } from 'react-type-animation'
 
 const Home = () => (
   <>
@@ -11,9 +12,12 @@ const Home = () => (
     <Container maxW="full">
       <AbsoluteCenter w="100%">
         <Text fontSize={{ base: "5xl", md: "7xl" }} textAlign="center">
-          Find{" "}
-          <span style={{ color: "white", background: "#276749" }}>local</span>{" "}
-          rental homes
+          <TypeAnimation
+            sequence={["Find local rental ", 2000, "Manage online ", 2000 ]}
+            repeat={Infinity}
+            cursor={false}
+          />{' '} 
+          <span style={{ color: "white", background: "#276749" }}>homes</span>{". "}
         </Text>
       </AbsoluteCenter>
     </Container>

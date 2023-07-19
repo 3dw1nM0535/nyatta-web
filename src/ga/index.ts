ea44a62742a1a941f8c62c33352ebec09c38903b
3dw1nM0535 <workockmoses@gmail.com>
2023-07-19 09:01:04 +0300

    fix: type

--- src/ga/index.ts
+++ src/ga/index.ts
@@ -1,9 +1,9 @@
 import ReactGA from 'react-ga4'
 
-export const trackPageView = ({ url, title }) =>
+export const trackPageView = ({ url, title }: any) =>
   ReactGA.send({ hitType: "pageview", page: url, title })
 
-export const trackEvent = ({ category, action, label, value }) =>
+export const trackEvent = ({ category, action, label, value }: any) =>
   ReactGA.event({ category, action, label, value })
 
-export const gaSetUserId = (userId) => ReactGA.set({ userId })
+export const gaSetUserId = (userId: string) => ReactGA.set({ userId })

293e5d560738b72b6d034eaa575b25b752de2bf1
3dw1nM0535 <workockmoses@gmail.com>
2023-07-20 03:25:41 +0300

    import fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { PropsWithChildren } from "react";
 
-import Layout from "@layout";
 import { Analytics } from '@vercel/analytics/react'
 import { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
 
+import Layout from "layout/index";
 import AppProvider from "providers/app-provider";
 import ProtectedPage from "providers/protected-page";
 import Providers from "providers/root";

7085c94bc7bb1d251990c9fa27b489ef2bf90984
3dw1nM0535 <workockmoses@gmail.com>
2023-08-02 12:59:13 +0300

    more fixes

--- src/views/listings/components/units.tsx
+++ src/views/listings/components/units.tsx
@@ -3,7 +3,7 @@
 import { useMemo } from 'react'
 
 import { useQuery } from '@apollo/client'
-import { Box, Badge, Center, Flex, Icon, SimpleGrid, Text } from '@chakra-ui/react'
+import { Box, Badge, Center, Flex, Icon, Text } from '@chakra-ui/react'
 import { MdImageNotSupported } from 'react-icons/md'
 
 import { GET_PROPERTY_UNITS } from '@gql'
@@ -25,39 +25,38 @@ const Units: React.FC = () => {
   if (unitsLoading ) return <Loader />
 
   return (
-    <SimpleGrid columns={[1, null, 3]}>
-      <Box w={{base: "full", md: 60}}>
-        {units.length > 0 && units.map((unit: any, index: number) => (
-          <Center key={index}>
-            <Box p="5" w="100%"borderWidth="1px">
-              <Center>
-                <Icon
-                  as={MdImageNotSupported}
-                  boxSize="10em"
-                  color="gray.200"
-                />
-              </Center>
-              <Flex align="baseline">
-                <Text
-                  textTransform="uppercase"
-                  fontWeight="bold"
-                  fontSize="sm"
-                >
-                  {unit.name}
-                </Text>
-                <Badge ml={2} colorScheme="green">{unit.state}</Badge>
-                <Text overflow="wrap" noOfLines={[1,2]} ml={2}>{`Amenities(${unit.amenityCount})`}</Text>
-              </Flex>
-              <Text mt={2} fontSize="sm" fontWeight="bold">{unitType(unit.type)}</Text>
-              <Text mt={2}>{`${new Intl.NumberFormat("en-KE", { style: "currency", currency: "KES" }).format(unit.price)}`}/month</Text>
-            </Box>
-          </Center>
-        ))}
-        {units.length === 0 && (
-          <Flex justifyContent={{base: "center", md: "flex-start"}}>No Listings</Flex>
-        )}
-      </Box>
-    </SimpleGrid>
+    <Box>
+      {units.length > 0 && units.map((unit: any, index: number) => (
+        <Center key={index}>
+          <Box p="5" w="320px" borderWidth="1px">
+            <Center>
+              <Icon
+                as={MdImageNotSupported}
+                boxSize="10em"
+                color="gray.200"
+              />
+            </Center>
+            <Flex align="baseline">
+              <Text
+                textTransform="uppercase"
+                fontWeight="bold"
+                fontSize="sm"
+                noOfLines={[1,2,3]}
+              >
+                {unit.name}
+              </Text>
+              <Badge ml={2} colorScheme="green">{unit.state}</Badge>
+              <Text noOfLines={[1,2,3]} ml={2}>{`Amenities(${unit.amenityCount})`}</Text>
+            </Flex>
+            <Text mt={2} fontSize="sm" fontWeight="bold">{unitType(unit.type)}</Text>
+            <Text mt={2}>{`${new Intl.NumberFormat("en-KE", { style: "currency", currency: "KES" }).format(unit.price)}`}/month</Text>
+          </Box>
+        </Center>
+      ))}
+      {units.length === 0 && (
+        <Flex justifyContent={{base: "center", md: "flex-start"}}>No Listings</Flex>
+      )}
+    </Box>
   )
 }
 
